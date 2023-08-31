747246c6b98b10193207f7f4136abbbabb24ec69
JOY19ANYANGO <joyanyango402@gmail.com>
2023-09-01 00:33:55 +0300

    done

---
+++ lib/migrations/versions/8cd5ea09ca49_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8cd5ea09ca49
+Revises: 
+Create Date: 2023-09-01 00:16:59.498629
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8cd5ea09ca49'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
